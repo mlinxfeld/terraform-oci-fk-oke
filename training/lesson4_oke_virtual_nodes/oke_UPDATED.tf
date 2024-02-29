4f6a57d970fa6ac1d2082f7cece842c2ec7ff745
mlinxfeld <martin.linxfeld@foggykitchen.com>
2024-02-29 13:48:37 +0000

    Fixed network for lesson4

--- training/lesson4_oke_virtual_nodes/oke_UPDATED.tf
+++ training/lesson4_oke_virtual_nodes/oke_UPDATED.tf
@@ -9,7 +9,7 @@ module "fk-oke" {
   use_existing_vcn              = false
   is_api_endpoint_subnet_public = true # OKE API Endpoint will be public (Internet facing)
   is_lb_subnet_public           = true # OKE LoadBalanacer will be public (Internet facing)
-  is_nodepool_subnet_public     = true # OKE NodePool will be public (Internet facing)
+  is_nodepool_subnet_public     = false # OKE NodePool will be private (Internet facing)
   virtual_node_pool             = true # Using Virtual Node Pool instead of Managed Node Pool
   pool_name                     = var.pool_name
   virtual_nodepool_pod_shape    = var.virtual_nodepool_pod_shape  
